a3a7e1957daecfdbe1dd786422aff629be5d4298
WSO <wirelesssolutinengineering@gmail.com>
2026-02-09 11:57:51 +0700

    Update IRIS Device

--- tasks/intersite_celery.py
+++ tasks/intersite_celery.py
@@ -486,6 +486,10 @@ def task_ipl(self, data: dict):
         lines = gpd.read_parquet(lines_path)
 
         self.update_state(state="PROGRESS", meta={"status": "Processing BOQ data"})
+
+        print(f"Device in Site: {device_in_site}")
+        print(f"Device in Branch: {device_in_branch}")
+        
         result = main_boq(
             points=points,
             lines=lines,
